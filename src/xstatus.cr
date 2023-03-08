cd7f2ba039450767b9500de852081da971b23e32
Pragu3 <6oxt1xhr@duck.com>
2023-03-07 19:50:48 -0500

    various changes

--- src/xstatus.cr
+++ src/xstatus.cr
@@ -2,40 +2,49 @@ require "x11"
 require "logger"
 require "./config"
 
-def set_status(dpy, status)
-    dpy.store_name dpy.default_root_window, status
-    dpy.flush
-end
+class Program
+    getter dpy : X11::Display
 
-def terminate(dpy, code)
-    set_status dpy, ""
-    exit 0
-end
-
-begin
-    dpy = X11::Display.new
-rescue ex
-    Logger.errorln ex; exit -1
-end
-
-# This makes sure the status is set back to an empty String when
-# The program is terminated.
-Signal::INT.trap do
-    terminate dpy, 0
-end
-
-include Config
-
-while true
-    begin
-        status = [] of String
-        CONFIG.each do |c|
-            status << c.function.call(c.format, c.arguments)
-        end
-
-        set_status dpy, status.join(" | ")
-    rescue ex
-        Logger.errorln ex; exit -1
+    def initialize
+        @dpy = X11::Display.new
     end
-    sleep 1.second
+
+    def set_status(status)
+        @dpy.store_name dpy.default_root_window, status
+        @dpy.flush
+    end
+
+    def terminate(code)
+        set_status ""
+        exit 0
+    end
+
+    include Config
+    def run
+    # This makes sure the status is set back to an empty String when
+    # The program is terminated.
+    Signal.each do |s|
+        if s == Signal::CHLD; break end
+        s.trap do
+            terminate 0
+        end
+    end
+
+    while true
+        begin
+            status = [] of String
+            CONFIG.each do |c|
+                status << c.function.call(c.format, c.arguments)
+            end
+
+            set_status status.join(" | ")
+        rescue ex
+            Logger.errorln ex; exit -1
+        end
+        sleep 1.second
+    end
+    end
 end
+
+p = Program.new
+p.run
